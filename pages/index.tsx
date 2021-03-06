3b53fdcd2b5ccc9a3448e8ff048c7f1572525140
Yuki Okushi <huyuumi.dev@gmail.com>
2021-03-07 04:05:44 +0900

    Fix HTML validator warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import React, {FC} from 'react'
-import Head from 'next/head'
+import React, { FC } from "react";
+import Head from "next/head";
 
 const Home: FC = () => {
   return (
@@ -11,18 +11,33 @@ const Home: FC = () => {
 
       <div className="flex items-center justify-center h-screen bg-gray-600">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-10 max-w-screen-lg">
-          <img className="w-5/12 md:w-8/12 rounded-full mx-auto" src="https://github.com/JohnTitor.png" alt="avatar"/>
+          <img
+            className="w-5/12 md:w-8/12 rounded-full mx-auto"
+            src="https://github.com/JohnTitor.png"
+            alt="avatar"
+          />
           <div className="mx-auto">
             <h2 className="text-3xl text-gray-100 mx-auto">Yuki Okushi</h2>
-            <p className="text-2xl text-gray-300 mx-auto">FLOSS nerd, Rustacean</p>
-            <p className="text-xl text-gray-100 mx-auto my-10">About me: Japanese codewriting ninja🥷</p>
-            <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded my-10"><a href="https://github.com/JohnTitor" target="_blank" rel="noopener noreferer">GitHub</a></button>
+            <p className="text-2xl text-gray-300 mx-auto">
+              FLOSS nerd, Rustacean
+            </p>
+            <p className="text-xl text-gray-100 mx-auto my-10">
+              About me: Japanese codewriting ninja🥷
+            </p>
+            <form
+              style={{display: "inline"}}
+              action="https://github.com/JohnTitor"
+              method="get"
+            >
+              <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded my-10">
+                GitHub
+              </button>
+            </form>
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Home;
