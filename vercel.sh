f426d6b74e0e16ec2bc4bcf06654fea63b2e35bc
Yuki Okushi <jtitor@2k36.org>
2021-07-24 07:20:06 +0900

    Use the "Ignored Build Step" feature

---
+++ vercel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+
+if [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ ^\[skip[[:space:]]ci\].* ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+fi
