92f07c43e22f59cb7ba6390fca2592be27cd793b
Yuki Okushi <jtitor@2k36.org>
2022-05-10 01:36:17 +0900

    Fix vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
-
-if [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ ^\[skip[[:space:]]ci\].* ]] ; then
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
-else
-  # Proceed with the build
-  echo "✅ - Build can proceed"
-  exit 1;
-fi
-
 if [[ "$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')" == "main" ]] ; then
   exit 0;
 fi
